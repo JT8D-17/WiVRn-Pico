01018266010a2562802e113eebd88e95288ff177
JT8D-17 <amusing.random.alias@gmail.com>
2024-08-23 21:51:17 +0200

    Modify APK keystore generation script

--- y_generate_apk_key.sh
+++ y_generate_apk_key.sh
@@ -4,5 +4,11 @@ export JAVA_HOME=/usr/lib/jvm/java-17-openjdk/
 
 keytool -genkey -v -keystore ks.keystore -alias default_key -keyalg RSA -keysize 2048 -validity 10000
 
-echo signingKeyPassword="123456" > gradle.properties
+echo " "
+echo " "
+read -r -s -p "Type in your keystore password: " passwd
 
+echo signingKeyPassword="$passwd" > gradle.properties
+
+echo " "
+echo "Keystore generated."
